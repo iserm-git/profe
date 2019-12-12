cccbde4e5a5d223c424247ac6c5c6b85776dc724
redgarm <redgarm@gmail.com>
2019-12-11 19:04:30 -0600

    Begin Sesion 2

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,30 +1,65 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import {
-  Container,
-  Button,
-  AppBar,
-  Toolbar,
-  IconButton,
-  InputBase,
-  Badge,
-  MenuItem,
-  Menu
-} from "@material-ui/core";
+import { Container, Button, TextField } from "@material-ui/core";
 
-import {
-  MenuIcon,
-  SearchIcon,
-  AccountCircle,
-  MailIcon,
-  NotificationsIcon,
-  MoreIcon
-} from "@material-ui/icons";
+// import {
+//   MenuIcon,
+//   SearchIcon,
+//   AccountCircle,
+//   MailIcon,
+//   NotificationsIcon,
+//   MoreIcon
+// } from "@material-ui/icons";
 
 class Login extends Component {
-  state = {};
+  state = {
+    hardcodedUser: {
+      user: "iserm",
+      password: "123"
+    },
+    usuario: "",
+    password: ""
+  };
+
+  handleInputUser = event => {
+    const usuario = event.target.value;
+    this.setState({ usuario: usuario });
+  };
+
+  handleSubmit = () => {
+    console.log(this.state);
+  };
+
   render() {
-    return <h1>Iniciar Sesion</h1>;
+    return (
+      <Container maxWidth="sm">
+        <form>
+          <TextField
+            id="user"
+            label="Usuario"
+            onChange={evento => this.handleInputUser(evento)}
+            value={this.state.usuario}
+          />
+          <br />
+          <TextField
+            id="password"
+            label="Contraseña"
+            type="password"
+            value={this.state.password}
+          />
+          <br />
+          <br />
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleSubmit}
+          >
+            Iniciar sesion
+          </Button>
+        </form>
+      </Container>
+    );
   }
 }
 
